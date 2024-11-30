5225ba8133cf337ba0f705329f827f2ec6c08aea
Jordan Davis <jtdavis@aurorasolar.com>
2024-11-29 20:35:18 -0500

    form

--- src/blocks/contact.js
+++ src/blocks/contact.js
@@ -17,11 +17,65 @@ function TextInput({ type = "text", label }) {
   );
 }
 
+function TextAreaInput({ label }) {
+  const [value, setValue] = useState("");
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return (
+    <div className="input-container textarea-container">
+      <textarea value={value} onChange={handleChange} />
+      <label className={value && "filled"} htmlFor="name">
+        {label}
+      </label>
+    </div>
+  );
+}
+
+function Radio() {
+  const [value, setValue] = useState("");
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return (
+    <div className="radio-container">
+      <p>Preferred communication</p>
+      <div class="switch-field">
+        <input
+          type="radio"
+          id="radio-one"
+          name="switch-one"
+          value="email"
+          checked
+          onChange={handleChange}
+        />
+        <label for="radio-one">Email</label>
+        <input
+          type="radio"
+          id="radio-two"
+          name="switch-one"
+          value="text"
+          onChange={handleChange}
+        />
+        <label for="radio-two">Text</label>
+      </div>
+    </div>
+  );
+}
+
 function Contact() {
   return (
     <div className="contact">
       <form>
-        <TextInput label="Name" />
+        <TextInput label="Full Name" />
+        <TextInput label="Phone Number" />
+        <TextInput label="Email" />
+        <Radio />
+        <TextAreaInput label="I'm interested in..." />
       </form>
     </div>
   );
